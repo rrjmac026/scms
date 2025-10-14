b9559dc78610531b38cd1ef43c93dee9c6c06007
unknown <https://github.com/Jam026/clients_final.git>
2025-10-14 20:44:12 +0800

    no validate

--- resources/views/admin/students/create.blade.php
+++ resources/views/admin/students/create.blade.php
@@ -11,7 +11,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6">
-                    <form action="{{ route('admin.students.store') }}" method="POST" class="space-y-8">
+                    <form action="{{ route('admin.students.store') }}" method="POST" class="space-y-8" novalidate>
                         @csrf
                         
                         <!-- User Account Information -->
